964c27db43f9690ff7a614f4fc29e8eb529d9974
usumaniana <usumaniana@pc10>
2022-10-04 13:36:37 +0200

    Base

---
+++ src/act02/grupos/materias/Act02GruposMaterias.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package act02.grupos.materias;
+
+/**
+ *
+ * @author usumaniana
+ */
+public class Act02GruposMaterias {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("prueba");
+    }
+    
+}
