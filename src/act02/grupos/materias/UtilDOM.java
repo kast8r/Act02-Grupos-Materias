645ac511cf1038dc84979088ad4510c42501fce3
usumaniana <usumaniana@pc10>
2022-10-04 14:00:29 +0200

    V1.0.1 Se ha añadido claseIsmael

---
+++ src/act02/grupos/materias/UtilDOM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package act02.grupos.materias;
+
+/**
+ *
+ * @author usumaniana
+ */
+public class UtilDOM {
+    private void claseIsmael() {
+
+    }
+    
+}
