b939de9856dbaca1fa46a8dfa9978dbb2e5567b7
usumaniana <usumaniana@pc09>
2022-10-04 13:40:35 +0200

    V1.0 Se han añadido clases UtilFTexto,UtilFBinario.

---
+++ src/act02/grupos/materias/UtilFBinario.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package act02.grupos.materias;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author usumaniana
+ */
+public class UtilFBinario {
+    
+    public static void guardarObjeto(Object Objeto, String nombreFichero){
+    File fichero = new File (nombreFichero);
+    FileOutputStream fos = null;
+    ObjectOutputStream salida = null;
+    
+        try {
+            fos = new FileOutputStream(fichero);
+            salida = new ObjectOutputStream(fos);
+            salida.writeObject(Objeto);                       
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(UtilFBinario.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(UtilFBinario.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            
+        try {
+            if (fos != null){
+                 fos.close();
+         }
+         
+             if (salida != null){
+                salida.close();
+         }
+        
+        
+        } catch (IOException ex) {
+            Logger.getLogger(UtilFBinario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        }
+    
+    }
+    
+    public static Object leerObjeto(String nombreFichero){
+    File fichero = new File (nombreFichero);
+    FileInputStream fis = null;
+    ObjectInputStream ois = null;
+    Object objeto = null;
+    
+        try {
+            fis= new FileInputStream(fichero);
+            ois= new ObjectInputStream(fis);
+            objeto = (Object) ois.readObject();
+            
+            
+            
+            
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(UtilFBinario.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(UtilFBinario.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UtilFBinario.class.getName()).log(Level.SEVERE, null, ex);
+        }finally {
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+                if (ois!= null) {
+                    ois.close();
+                }
+            } catch (IOException e) {
+                     System.out.println(e.getMessage());
+            }
+        
+    
+    
+    return objeto;
+    }
+  
+        }
+
+}
---
+++ src/act02/grupos/materias/UtilFTexto.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package act02.grupos.materias;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author usumaniana
+ */
+public class UtilFTexto {
+    
+    
+    public void escribirTxt(String texto, String fichero){
+    FileWriter fw;
+        try {
+            fw = new FileWriter(fichero);
+            fw.write(texto);
+            fw.close();
+             
+        } catch (IOException ex) {
+            Logger.getLogger(UtilFTexto.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        
+    }
+    
+     
+    public String leerTxt(String fichero){
+        String result="";
+        String linea=null;      
+        FileReader fr=null;
+        BufferedReader br=null;
+        
+             try {
+                 fr=new FileReader(fichero);
+                 br=new BufferedReader(fr);
+                 
+                while ((linea=br.readLine()) != null){                        
+                 result=result+"\n"+linea;
+            }
+                                
+                 
+             } catch (FileNotFoundException ex) {
+                 Logger.getLogger(UtilFTexto.class.getName()).log(Level.SEVERE, null, ex);
+               } catch (IOException ex) {
+            Logger.getLogger(UtilFTexto.class.getName()).log(Level.SEVERE, null, ex);
+             }finally {
+                
+                 try {
+                   br.close();
+             
+             }catch (IOException ex) {
+            Logger.getLogger(UtilFTexto.class.getName()).log(Level.SEVERE, null, ex);
+             
+             }
+             }
+        
+                
+           return result;
+        }  
+    }
+
+           
+    
+    
+    
+    
+    
+    
+
