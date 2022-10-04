dbe02b6a3ed9eef7f6dbcc6940ded8eeba4f0e9e
usumaniana <usumaniana@pc10>
2022-10-04 14:11:59 +0200

    V1.2 Se ha actualizado la clase UtilDom

--- src/act02/grupos/materias/UtilDOM.java
+++ src/act02/grupos/materias/UtilDOM.java
@@ -4,13 +4,66 @@
  */
 package act02.grupos.materias;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 /**
  *
  * @author usumaniana
  */
 public class UtilDOM {
-    private void claseIsmael() {
-
+    
+    public Document crearDOM() {
+        Document result = null;
+        
+        return result;
     }
     
+    public Document crearRaiz(Document doc, String etiquetaRaiz) {
+        Document result = null;
+        
+        return result;
+    }
+        
+    public Element getRaiz(Document doc) {
+        Element result = null;
+        
+        return result;
+    }
+    
+
+    
+    /**
+     * Crea Elemento sin texto
+     * @param padre
+     * @param doc 
+     */
+    public void crearElemento(String etiqueta, Element padre, Document doc){
+        
+    }
+    
+    
+    
+    /**
+     * Crea un elemento con texto
+     * @param etiqueta
+     * @param padre
+     * @param doc 
+     */
+    public void crearElemento (String etiqueta, String texto, Element padre, Document doc){
+        
+    }
+    
+    
+        
+    /**
+     * Crea un element con texto y con un atributo
+     * @param etiqueta
+     * @param texto
+     * @param padre
+     * @param doc 
+     */
+    public void crearElemento (String etiqueta, String texto, String atributo, String textoAtr, Element padre,Document doc) {
+        
+    }
 }
