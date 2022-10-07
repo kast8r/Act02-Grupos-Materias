638119264d72c9c69ec0119c24eb4c1bf6a219ae
usumaniana <usumaniana@pc10>
2022-10-07 13:33:04 +0200

    V1.5.1.I Se han creado e implementado las clases modelo

---
+++ src/Modelo/Materia.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author usumaniana
+ */
+public class Materia {
+    String nombre,abreviatura;
+    int clave;
+
+    public Materia(String nombre, String abreviatura, int clave) {
+        this.nombre = nombre;
+        this.abreviatura = abreviatura;
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getAbreviatura() {
+        return abreviatura;
+    }
+
+    public int getClave() {
+        return clave;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setAbreviatura(String abreviatura) {
+        this.abreviatura = abreviatura;
+    }
+
+    public void setClave(int clave) {
+        this.clave = clave;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "nombre=" + nombre + ", abreviatura=" + abreviatura + ", clave=" + clave + '}';
+    }
+    
+    
+}
